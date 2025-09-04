63c2c646c9e75d45e9098ccbf62d6fa1c6bc6fec
Hunter Melrose <huntermelrose831@gmail.com>
2025-09-04 12:17:30 -0700

    first submission

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -15,6 +16,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", mainRouter);
 
